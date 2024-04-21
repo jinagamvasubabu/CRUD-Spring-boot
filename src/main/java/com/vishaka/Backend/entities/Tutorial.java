01805ee336a2a26ded2f56901ea642fb31965340
hangoutdude <jinagamvasubabu@gmail.com>
2024-04-21 21:23:52 +0530

    initial commit

---
+++ src/main/java/com/vishaka/Backend/entities/Tutorial.java
@@ -0,0 +1,31 @@
+package com.vishaka.Backend.entities;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tutorials")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tutorial {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @Column(name = "title")
+  private String title;
+
+  @Column(name = "description")
+  private String description;
+
+  @Column(name = "published")
+  private boolean published;
+
+  public Tutorial(String title, String description, boolean published) {
+    this.title = title;
+    this.description = description;
+    this.published = published;
+  }
+}
